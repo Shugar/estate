691f714ca2c4ace2e113069725e3bb94338fc0f8
Tigran Tumasov <shugar348@gmail.com>
2016-10-01 04:57:11 +0300

    add sliders, also head search form added

--- app/scripts/initialize.js
+++ app/scripts/initialize.js
@@ -1,3 +1,29 @@
 $(document).ready(() => {
-  console.log('Ready!');
+  $('.suggestions-list').slick({
+    dots: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.additional-list').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    customPaging : function(slider, i) {
+        var thumb = $(slider.$slides[i]).find('.title').text();
+        return '<div class="custom-dot">'+ thumb + '</div>';
+    }
+  });
 });
